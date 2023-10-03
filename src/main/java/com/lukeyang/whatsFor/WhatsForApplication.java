f58d221949d8b67b601aabc428cbfd7c13f6e196
lukeyang01 <lukeyang@umich.edu>
2023-10-02 20:50:38 -0400

    re init

---
+++ src/main/java/com/lukeyang/whatsFor/WhatsForApplication.java
@@ -0,0 +1,13 @@
+package com.lukeyang.whatsFor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsForApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsForApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lukeyang/whatsFor/WhatsForApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukeyang.whatsFor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsForApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
